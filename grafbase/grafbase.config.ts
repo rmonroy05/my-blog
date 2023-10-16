80c726335df37547584fd60aec26934cc471f010
rebecca monroy <monroy.rebecca5@gmail.com>
2023-10-16 15:28:17 -0600

    fixed db 2

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,19 +1,18 @@
 import { g, config, auth } from '@grafbase/sdk';
 
-// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
+  linkedInUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
 }).auth((rules) => {
   rules.public().read()
 })
 
-// @ts-ignore
+
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
